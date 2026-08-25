0a97d722ccb43bdd970994bc496eed548316677c
Starinin Andrey <starininandrey@gmail.com>
2026-08-25 21:09:36 +0300

    Update 4

--- ViewModels/Pages/Page1ViewModel.cs
+++ ViewModels/Pages/Page1ViewModel.cs
@@ -4,7 +4,14 @@ namespace SPW.ViewModels;
 
 public partial class Page1ViewModel : PageViewModelBase
 {
+    [Reactive] private bool _isEnabled = false;
     public Page1ViewModel(string text) 
         : base(text)
     { }
+
+    [ReactiveCommand]
+    private void Edit()
+    {
+        IsEnabled = true;
+    }
 }
